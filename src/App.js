9811b92ad12b5a0fb0f34355808a466b67f2149f
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2017-03-17 21:25:04 +0400

    basic layout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='filtered'>
+            <div>
+            <h1> Filtered Lists </h1>
+              <input type="text" className="search" placeholder="Filter...">
+              </input>
+            </div>
+
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const countriesFile ='./countries.js'
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
