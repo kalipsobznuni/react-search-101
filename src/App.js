c958f679ea606ebf5511ca08ff1dd3dea419cacb
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2017-03-21 18:54:59 +0400

    full functionality

--- src/App.js
+++ src/App.js
@@ -2,31 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: " "
+    }
+  }
+
+    changeSearch = (e) => {
+      const search = e.currentTarget.value.toLowerCase();
+      this.setState({ search });
+    }
+
   render() {
 
-const {data} = this.props;
+    const {data} = this.props;
+    const {search} = this.state;
+    const countriesList = data.map((erkir, idx)=>{
+      //console.log(search)
+      if(erkir.name.toLowerCase().includes(search)) {
+        return <div className='list' key={idx}> {name} </div>
+   } else {
+     //console.log(erkir.name)
+      return(
+        <div key={idx}>{erkir.name}</div>
 
-const countriesList = data.map(name=>{
-  return(
-    <li>{name.name}</li>
-  )
+      )}
+    });
 
-});
-
-
-  return (
+    return (
       <div className="App">
         <div className='filtered'>
-            <div>
-            <h1> Filtered Lists </h1>
-              <input type="text" className="search" placeholder="Filter...">
-              </input>
-              {countriesList}
-              </div>
-          </div>
-      </div>
-    );
-  }
+          <div>
+          <h1> Filtered Lists </h1>
+            <input type="text"
+              className="search"
+              placeholder="Filter..."
+              value={this.state.search}
+              onChange={(e) => this.setState({search: e.currentTarget.value})}
+              />
+              <div className='list'>{countriesList}</div>
+        </div>
+    </div>
+</div>
+);
+}
 }
 
 export default App;
+
+/*
+const countriesList= data.filter(
+  (country)=>{
+    return data.name.indexOf(this.state.search) !== -1;
+  }
+)
+*/
